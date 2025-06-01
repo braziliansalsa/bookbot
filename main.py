0ca5a68ca7da94165193c79edfb4db91364a510a
braziliansalsa <spicybraziliansalsa@gmail.com>
2025-06-01 16:46:36 +0200

    add count_characters function and update main.py to display character count

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import count_words
+from stats import count_words, count_characters
 def main():
     path_to_file = "books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
     num_words = count_words(book_text)
+    dict_characters = count_characters(book_text)
     print(f"{num_words} words found in the document")
-
+    print(f"{dict_characters} characters found in the document")
+    
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def count_words(file_contents):
     words = file_contents.split()
     return len(words)
+
+def count_characters(file_contents):
+    characters = file_contents.lower()
+    dict_characters = {}
+    for char in characters:
+        dict_characters[char] = dict_characters.get(char, 0) + 1
+    return dict_characters
